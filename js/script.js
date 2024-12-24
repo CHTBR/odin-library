db7c2ab6f4da13a245bac386acc9b932d5c03f3c
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-12-24 12:54:01 +0100

    Add script for handling form data, adding a new book and displaying it

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Library managment */
 let myLibrary = [];
 
 function Book(name, author, numOfPages, haveRead) {
@@ -14,6 +15,8 @@ function addBook(name, author, numOfPages, haveRead) {
   myLibrary.push(new Book(name, author, numOfPages, haveRead));
 }
 
+
+/* Book display managment */
 const body = document.body;
 
 function displayAllBooks() {
@@ -43,3 +46,29 @@ function displayBook(book) {
 
   body.appendChild(bookCard);
 }
+
+
+/* Book adding form managment */
+const dialog = document.getElementsByTagName("dialog")[0];
+const form = document.getElementsByTagName("form")[0];
+const nameInput = document.getElementById("bookName");
+const authorInput = document.getElementById("bookAuthor");
+const pagesInput = document.getElementById("bookNumOfPages");
+const haveReadInput = document.getElementById("bookHaveRead")
+const openDialogBtn = document.getElementsByClassName("new-book-button")[0];
+const closeDialogBtn = document.getElementsByClassName("dialog-close-button")[0];
+
+openDialogBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeDialogBtn.addEventListener("click", () => {
+  dialog.close();
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addBook(nameInput.value, authorInput.value, pagesInput.value, haveReadInput.value);
+  displayBook(myLibrary[myLibrary.length - 1]);
+  dialog.close();
+});
