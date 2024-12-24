e4b38727da8da2ef3ab56e1af5ad85935104901d
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-12-24 11:48:59 +0100

    Add script for displaying each book in the library on the webpage

--- js/script.js
+++ js/script.js
@@ -13,3 +13,33 @@ function Book(name, author, numOfPages, haveRead) {
 function addBook(name, author, numOfPages, haveRead) {
   myLibrary.push(new Book(name, author, numOfPages, haveRead));
 }
+
+const body = document.body;
+
+function displayAllBooks() {
+  myLibrary.forEach(displayBook);
+}
+
+function displayBook(book) {
+  let bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+
+  let bookName = document.createElement("h2");
+  bookName.textContent = book.name;
+
+  let bookAuthor = document.createElement("p");
+  bookAuthor.textContent = "Author: " + book.author;
+
+  let bookNumOfPages = document.createElement("p");
+  bookNumOfPages.textContent = "Number of pages: " + book.numOfPages;
+
+  let bookHaveRead = document.createElement("p");
+  bookHaveRead.textContent = "Status: " + (book.haveRead ? "haven't read yet" : "already read");
+
+  bookCard.appendChild(bookName);
+  bookCard.appendChild(bookAuthor);
+  bookCard.appendChild(bookNumOfPages);
+  bookCard.appendChild(bookHaveRead);
+
+  body.appendChild(bookCard);
+}
