5ff36eb04c91d0a29ffb49f1c9bb07f2ca45fecb
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2025-11-28 15:48:00 +0100

    Add dialog and script for adding books

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 const bookContainer = document.getElementsByClassName("book-container")[0];
+const addBookDialog = document.getElementsByClassName("add-book")[0];
+const addBookForm = addBookDialog.querySelector("form");
 
 const myLib = [];
 
@@ -15,7 +17,31 @@ function addBookToLib(title, author, numOfPages, haveRead) {
   myLib.push(book);
 }
 
+function openAddBookDialog() {
+  addBookDialog.showModal();
+}
+
+function closeAddBookDialog() {
+  addBookDialog.close();
+}
+
+function processAddBookForm() {
+  let children = addBookForm.children;
+  let title = children[0].value;
+  let author = children[1].value;
+  let pages = children[2].value;
+  let status = children[3].checked;
+  console.log(`${title} ${author} ${pages} ${status}`);
+  closeAddBookDialog();
+  addBookToLib(title, author, pages, status);
+  renderBooks();
+}
+
 function renderBooks() {
+  while (bookContainer.hasChildNodes()) {
+    bookContainer.removeChild(bookContainer.childNodes[0]);
+  }
+
   myLib.forEach(function (bookData) {
     let book = document.createElement("div");
     book.classList.add("book-card");
@@ -35,8 +61,6 @@ function renderBooks() {
     book.appendChild(status);
 
     bookContainer.appendChild(book);
-
-    console.log("Added book!");
   });
 }
 
