8cd5e6325a971a11b75eed93413e38d3456c4c7d
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2025-11-28 08:36:37 +0100

    Add way to render books and Remove template

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+const bookContainer = document.getElementsByClassName("book-container")[0];
+
 const myLib = [];
 
 function Book(title, author, numOfPages, haveRead) {
@@ -12,3 +14,30 @@ function addBookToLib(title, author, numOfPages, haveRead) {
   let book = new Book(title, author, numOfPages, haveRead);
   myLib.push(book);
 }
+
+function renderBooks() {
+  myLib.forEach(function (bookData) {
+    let book = document.createElement("div");
+    book.classList.add("book-card");
+
+    let title = document.createElement("h2");
+    title.textContent = bookData.title;
+    let author = document.createElement("p");
+    author.textContent = bookData.author;
+    let pages = document.createElement("p");
+    pages.textContent = bookData.numOfPages;
+    let status = document.createElement("p");
+    status.textContent = bookData.haveRead ? "Already read" : "Haven\'t read yet";
+
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(status);
+
+    bookContainer.appendChild(book);
+
+    console.log("Added book!");
+  });
+}
+
+renderBooks();
