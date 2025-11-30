020e2615ca15fe8d3eb6fd99d1a75c9f9afb3137
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2025-11-30 07:35:25 +0100

    Add table display and Add responsive font size

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
-const bookContainer = document.getElementsByClassName("book-container")[0];
+const main = document.getElementsByTagName("main")[0];
 const addBookDialog = document.getElementsByClassName("add-book")[0];
 const addBookForm = addBookDialog.querySelector("form");
 const themeToggle = document.getElementsByClassName("toggle-track theme")[0];
+const switchStyleButton = document.getElementsByClassName("switch-style-button")[0];
 
+let renderStyle = "cards";
 const myLib = [];
+
 
 function Book(title, author, numOfPages, haveRead) {
   this.id = crypto.randomUUID();
@@ -17,18 +20,21 @@ Book.prototype.toggleHaveRead = function () {
   this.haveRead = !this.haveRead;
 }
 
-function addBookToLib(title, author, numOfPages, haveRead) {
-  let book = new Book(title, author, numOfPages, haveRead);
-  myLib.push(book);
-}
 
 function openAddBookDialog() {
   addBookDialog.showModal();
 }
 
+
 function closeAddBookDialog() {
   addBookDialog.close();
 }
+
+function addBookToLib(title, author, numOfPages, haveRead) {
+  let book = new Book(title, author, numOfPages, haveRead);
+  myLib.push(book);
+}
+
 
 function processAddBookForm() {
   let inputChildren = addBookForm.querySelectorAll("input");
@@ -41,56 +47,129 @@ function processAddBookForm() {
   renderBooks();
 }
 
-function renderBooks() {
-  while (bookContainer.hasChildNodes()) {
-    bookContainer.removeChild(bookContainer.childNodes[0]);
-  }
 
+function renderBooks() {
+  if (main.querySelector(".book-container")) {
+    main.querySelector(".book-container").remove();
+  } if (main.querySelector("table")) {
+    main.querySelector("table").remove();
+  }
+  if (renderStyle == "cards") {
+    renderBooksAsCards();
+  } else if (renderStyle == "table") {
+    renderBooksAsTable();
+  }
+}
+
+
+function renderBooksAsCards() {
+  let bookContainer = document.createElement("div");
+  bookContainer.classList.add("book-container");
   myLib.forEach(function (bookData) {
     let book = document.createElement("div");
     book.classList.add("book-card");
-
     let title = document.createElement("h2");
     title.textContent = bookData.title;
     let author = document.createElement("p");
     author.textContent = bookData.author;
     let pages = document.createElement("p");
     pages.textContent = bookData.numOfPages;
-
     let removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
     removeButton.classList.add("danger-button");
     removeButton.addEventListener("click", () => {
       let index = myLib.findIndex((book) => book.id == bookData.id);
       myLib.splice(index, 1);
-      renderBooks();
+      bookContainer.removeChild(book);
     });
-
     let toggleStatusButton = document.createElement("button");
     toggleStatusButton.textContent = bookData.haveRead ? "Already read" : "Haven't read yet";
     toggleStatusButton.addEventListener("click", function () {
       bookData.toggleHaveRead();
       toggleStatusButton.textContent = bookData.haveRead ? "Already read" : "Haven't read yet";
-      renderBooks();
     });
-
     book.appendChild(title);
     book.appendChild(author);
     book.appendChild(pages);
     book.appendChild(toggleStatusButton);
     book.appendChild(removeButton);
-
     bookContainer.appendChild(book);
   });
+  main.appendChild(bookContainer);
 }
+
+
+function renderBooksAsTable() {
+  let bookTable = document.createElement("table");
+  let bookThead = document.createElement("thead");
+  let bookTheadRow = document.createElement("tr");
+  let bookTbody = document.createElement("tbody");
+  ["Name", "Author", "Number of pages", "Status", "Remove"].forEach(header => {
+    let bookTh = document.createElement("th");
+    bookTh.textContent = header;
+    bookTheadRow.appendChild(bookTh);
+  });
+  bookThead.appendChild(bookTheadRow);
+  bookTable.appendChild(bookThead);
+
+  myLib.forEach(function (bookData) {
+    let book = document.createElement("tr");
+    let title = document.createElement("th");
+    title.textContent = bookData.title;
+    let author = document.createElement("td");
+    author.textContent = bookData.author;
+    let pages = document.createElement("td");
+    pages.textContent = bookData.numOfPages;
+    let status = document.createElement("td");
+    let toggleStatusButton = document.createElement("button");
+    toggleStatusButton.textContent = bookData.haveRead ? "Already read" : "Haven't read yet";
+    toggleStatusButton.addEventListener("click", function () {
+      bookData.toggleHaveRead();
+      toggleStatusButton.textContent = bookData.haveRead ? "Already read" : "Haven't read yet";
+    });
+    status.appendChild(toggleStatusButton);
+    let remove = document.createElement("td");
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.classList.add("danger-button");
+    removeButton.addEventListener("click", () => {
+      let index = myLib.findIndex((book) => book.id == bookData.id);
+      myLib.splice(index, 1);
+      bookTbody.removeChild(book);
+    });
+    remove.appendChild(removeButton);
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(status);
+    book.appendChild(remove);
+    bookTbody.appendChild(book);
+  });
+  bookTable.appendChild(bookTbody);
+  main.appendChild(bookTable);
+}
+
 
 themeToggle.addEventListener("click", () => {
   themeToggle.classList.toggle("toggle-on");
   document.documentElement.classList.toggle("light-theme");
 });
 
+
+switchStyleButton.addEventListener("click", () => {
+  if (renderStyle == "cards") {
+    renderStyle = "table";
+    switchStyleButton.textContent = "Cards display";
+  } else if (renderStyle == "table") {
+    renderStyle = "cards";
+    switchStyleButton.textContent = "Table display";
+  }
+  renderBooks();
+});
+
+
 [['The fellowship of the ring', 'J.R.R. Tolkien', 432, false], ['The name of the wind', 'Patrick Rothfuss', 662, true], ['Eragon', 'Christopher Paolini', 503, true], ['Harry Potter and the Philosopher\'s stone', 'J.K. Rowling', 223, false]].forEach((el) => {
   addBookToLib(el[0], el[1], el[2], el[3]);
 });
 
-renderBooks();
+renderBooks()
